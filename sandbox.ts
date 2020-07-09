24eb149a3b891b7d71ee5ee76abca58a38cbfd34
SAFNAS <iamsafnas@gmail.com>
2020-07-09 22:00:24 +0530

    uid module

--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,5 @@
-//fetch api
+//uuid module
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
-const res = await fetch("https://swapi.dev/api/films/");
-const data = await res.json();
-
-console.log(data);
+const uid = v4.generate();
+console.log(uid);
