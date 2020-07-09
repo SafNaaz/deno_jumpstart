31e32e3013a3f8913302e941e606391117314370
SAFNAS <iamsafnas@gmail.com>
2020-07-09 23:48:31 +0530

    case module testing

--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,30 @@
-//http module
+// //http module
 
-import { serve } from "https://deno.land/std/http/server.ts";
+// import { serve } from "https://deno.land/std/http/server.ts";
 
-const server = serve({ port: 3000 });
-console.log("listening for requests on port 3000");
+// const server = serve({ port: 3000 });
+// console.log("listening for requests on port 3000");
 
-for await (const req of server) {
-  console.log("request made");
-  const url = req.url;
-  req.respond({ body: `Hello again ninjas, you visited ${url}` });
-}
+// for await (const req of server) {
+//   console.log("request made");
+//   const url = req.url;
+//   req.respond({ body: `Hello again ninjas, you visited ${url}` });
+// }
+
+import {
+  camelCase,
+  paramCase,
+  pascalCase,
+  snakeCase,
+} from "https://deno.land/x/case/mod.ts";
+
+const text = "hello again ninjas";
+
+const camel = camelCase(text);
+const param = paramCase(text);
+const pascal = pascalCase(text);
+const snake = snakeCase(text);
+
+console.log(camel, param, pascal, snake);
+
+//helloAgainNinjas hello-again-ninjas HelloAgainNinjas hello_again_ninjas
