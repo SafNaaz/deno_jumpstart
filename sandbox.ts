433fb9c7830201bd3aa8009e1fe51b5da69702fb
SAFNAS <iamsafnas@gmail.com>
2020-07-09 22:06:15 +0530

    readJson

--- sandbox.ts
+++ sandbox.ts
@@ -3,3 +3,10 @@ import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 const uid = v4.generate();
 console.log(uid);
+
+// fs module
+
+import { readJson, writeJson } from "https://deno.land/std/fs/mod.ts";
+
+const jsonObj = await readJson("ninjas.json");
+console.log(jsonObj);
