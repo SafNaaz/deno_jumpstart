d013834f26e1eaa6c77b7b0892fe1988e8d99547
SAFNAS <iamsafnas@gmail.com>
2020-07-09 17:42:11 +0530

    writing to files

--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,13 @@
+//reading files
+
 const decoder = new TextDecoder("utf-8");
 
 const data = await Deno.readFile("readme.txt");
 console.log(decoder.decode(data));
+
+//writing files
+
+const encoder = new TextEncoder();
+const text = encoder.encode("hello again, ninjas");
+
+await Deno.writeFile("readme.txt", text);
